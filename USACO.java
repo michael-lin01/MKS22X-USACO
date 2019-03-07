b6336c2866d4c0d23c75ac15a864b500429d084b
Michael Lin <mlin01@stuy.edu>
2019-03-07 10:03:41 -0500

    finished bronze problem

--- USACO.java
+++ USACO.java
@@ -20,10 +20,55 @@ public class USACO{
       }
     }
 
-    return 1;
+    /*
+    String ans = "";
+    for(int r = 0; r < rows; r++){
+      for(int c = 0; c < cols;c++){
+        if(map[r][c]<10) ans += " "+map[r][c]+" ";
+        else ans+= map[r][c]+" ";
+      }
+      ans+="\n";
+    }
+    System.out.println(ans);
+    */
+
+    for(int i = 0; i < numStomps; i++){
+      line=in.nextLine();
+      String[] stomps = line.split(" ");
+      stomp(Integer.parseInt(stomps[0])-1,Integer.parseInt(stomps[1])-1,Integer.parseInt(stomps[2]),map);
+    }
+    int sum = 0;
+    for(int[] r: map){
+      for(int c:r){
+        if(elevation-c>0) sum+=elevation-c;
+      }
+    }
+    return sum*72*72;
+  }
+
+  public static void stomp(int row, int col, int down, int[][] map){
+    int max = -1;
+    for(int r = row; r <= row+2; r++){
+      for(int c = col; c <= col+2; c++){
+        if(map[r][c]>max) max = map[r][c];
+      }
+    }
+    int elev = max-down;
+    for(int r = row; r <= row+2; r++){
+      for(int c = col; c <= col+2; c++){
+        if(map[r][c]>elev) map[r][c]=elev;
+      }
+    }
+
   }
 
   public static void main(String args[]){
-    
+    try{
+      System.out.println(bronze("./testCases/makelake.1.in"));
+    }
+    catch(FileNotFoundException e){
+      System.out.println("File not found");
+    }
+
   }
 }
